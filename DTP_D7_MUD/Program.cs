153628a671edc0f942b83f4be63c7952a65f74bb
Ludde Andersson <147702357+dradwul@user.noreply.github.com>
2023-10-30 17:53:57 +0100

    First commit

---
+++ DTP_D7_MUD/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DTP_D7_MUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
