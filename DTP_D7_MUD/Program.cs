aacf0809840e61922af6144de40c239ed1582e5d
Ludde Andersson <147702357+dradwul@user.noreply.github.com>
2023-10-30 18:34:08 +0100

    Lagt till Room-klass och kommando-printfunktion

--- DTP_D7_MUD/Program.cs
+++ DTP_D7_MUD/Program.cs
@@ -1,10 +1,34 @@
 ﻿namespace DTP_D7_MUD
 {
+    class Room
+    {
+        private string name;
+        private string presentation;
+        private int number;
+        private int north;
+        private int east;
+        private int south;
+        private int west;
+
+        public static void PrintCommands()
+        {
+            Console.WriteLine("ladda /file/   ladda en fil med rum");
+            Console.WriteLine("spara          spara en fil med rum");
+            Console.WriteLine("starta         gå till rum 0");
+            Console.WriteLine("n              gå norrut");
+            Console.WriteLine("s              gå söderut");
+            Console.WriteLine("e              gå österut");
+            Console.WriteLine("ö              gå österut");
+            Console.WriteLine("v              gå västerut");
+            Console.WriteLine("w              gå västerut");
+        }
+    }
+    
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Room.PrintCommands();
         }
     }
 }
